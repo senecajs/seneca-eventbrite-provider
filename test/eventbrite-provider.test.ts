c401178ad0d3305f90898bd8f668a5c38c40b356
Gustavo <guhmerces@hotmail.com>
2021-12-15 17:43:22 -0300

    add condition to test if auth key is provided

--- test/eventbrite-provider.test.ts
+++ test/eventbrite-provider.test.ts
@@ -51,36 +51,40 @@ describe('eventbrite-provider', () => {
   })
 
   test('entity-load', async () => {
-    const seneca = Seneca({ legacy:false })
-      .test()
-      .use('promisify')
-      .use('entity')
-      .use('provider', providerOptions)
-      .use(EventbriteProvider)
+    if(CONFIG.key) {
+      const seneca = Seneca({ legacy:false })
+        .test()
+        .use('promisify')
+        .use('entity')
+        .use('provider', providerOptions)
+        .use(EventbriteProvider)
       
-    const event = await seneca.entity('eventbrite/event').load$('214728557897');
-    expect(event).toBeDefined()
-    expect(event.id).toEqual('214728557897')
-    expect(event).toHaveProperty('name')
-    expect(event).toHaveProperty('description')
+      const event = await seneca.entity('eventbrite/event').load$('214728557897');
+      expect(event).toBeDefined()
+      expect(event.id).toEqual('214728557897')
+      expect(event).toHaveProperty('name')
+      expect(event).toHaveProperty('description')
+    }
   })
 
   test('entity-save', async () => {
-    const seneca = Seneca({ legacy:false })
-      .test()
-      .use('promisify')
-      .use('entity')
-      .use('provider', providerOptions)
-      .use(EventbriteProvider)
+    if(CONFIG.key) {
+      const seneca = Seneca({ legacy:false })
+        .test()
+        .use('promisify')
+        .use('entity')
+        .use('provider', providerOptions)
+        .use(EventbriteProvider)
     
-    let event = await seneca.entity('eventbrite/event').load$('228153231457')
+      let event = await seneca.entity('eventbrite/event').load$('228153231457')
 
-    const randomBytes = crypto.randomBytes(12).toString('hex')
-    
-    event.description.html = randomBytes    
-    event = await event.save$();
+      const randomBytes = crypto.randomBytes(12).toString('hex')
+      
+      event.description.html = randomBytes    
+      event = await event.save$();
 
-    expect(event.description.html).toEqual(randomBytes)
+      expect(event.description.html).toEqual(randomBytes)
+    }
   })
 
   test('messages', async () => {
