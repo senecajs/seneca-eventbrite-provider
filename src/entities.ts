ab0a428599013ff6073fd82265163bcb18f5b1a8
Gustavo <guhmerces@hotmail.com>
2022-01-17 14:13:33 -0300

    Create entities file
    Add category and discount entities to entities map

---
+++ src/entities.ts
@@ -0,0 +1,65 @@
+import crypto from 'crypto'
+
+const test_args = {
+    discount_id: '609282579',
+    category_id: '101',
+}
+
+const rand = crypto.randomBytes(10).toString('hex')
+
+const entities = {
+  category: {
+    fields: [],
+    commands: [
+      {
+        cmd: "load",
+        method: "get",
+        path: "/categories/:category_id/",
+        include: [],
+      },
+    ],
+    tests: {
+      load: {
+        args: {
+          category_id: test_args.category_id,
+        },
+        expectations: {
+          id: {
+            sameAs: test_args.category_id,
+          },
+        },
+      },
+    },
+  },
+  discount: {
+    fields: [],
+    commands: [
+      {
+        cmd: "load",
+        method: "get",
+        path: "/discounts/:discount_id/",
+        include: [],
+      },
+      {
+        cmd: "save",
+        method: "post",
+        path: "/discounts/:discount_id/",
+        include: [],
+      },
+    ],
+    tests: {
+      load: {
+        args: {
+          discount_id: test_args.discount_id,
+        },
+        expectations: {
+          id: {
+            sameAs: test_args.discount_id,
+          },
+        },
+      },
+    },
+  },
+}
+
+export { entities }
