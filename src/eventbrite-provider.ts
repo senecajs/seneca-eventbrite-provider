29c347a218f87481613536e39a92f6f16039760e
Gustavo <guhmerces@hotmail.com>
2022-01-18 06:28:49 -0300

    Change functions names and break them in small parts

--- src/eventbrite-provider.ts
+++ src/eventbrite-provider.ts
@@ -4,7 +4,7 @@ import Eventbrite from 'eventbrite'
 import { Sdk } from 'eventbrite/lib/types'
 import { cmd_handlers } from './cmd-handlers'
 import { entities as ent_map } from './entities'
-import { handle_request } from './helpers'
+import { request } from './helpers'
 
 type EventbriteProviderOptions = {}
 
@@ -34,33 +34,47 @@ function EventbriteProvider(this: any, options: any) {
           ...common,
         }
 
-        const action = init_request(command_details.path)
+        const initialized_req_handler = init_handler(command_details.path)
 
-        const handler = cmd_handlers(
-          action,
+        const cmd_handler = cmd_handlers(
+          initialized_req_handler,
           command_details.body_args,
           command_details.include,
           command_details.query_params
         )
 
-        seneca.message(pattern, handler)
+        seneca.message(pattern, cmd_handler)
       })
     })
   }
 
-  function init_request(path: string) {
-    return async(args: Record<string,any>, options: RequestInit) => {
-      const matches_obj = path.matchAll(new RegExp(/:(.[^\/]+)/g))
-      const matches_arr = [...matches_obj]
-
-      matches_arr.forEach(match => {
-        const url_placeholder = match[0]
-        const url_placeholder_name = match[1]
-        path = path.replace(url_placeholder, args[url_placeholder_name])
-      })
-      const handle = handle_request(eventbrite.request, path)
-      return handle(options)
+  function init_handler(path: string) {
+    return (path_args: Record<string,any>) => {
+      path = build_path(path, path_args)
+      const make_request = request(eventbrite.request, path)
+      return make_request
     }
+  }
+
+  function build_path(path: string, args: Record<string,any>) {
+    //match parameters after colon (:) in the path entity path
+    const matches_obj = path.matchAll(new RegExp(/:(.[^\/]+)/g))
+    const matches_arr = [...matches_obj]
+
+    matches_arr.forEach((match) => {
+      const url_placeholder = match[0]
+      const url_placeholder_name = match[1]
+
+      if (!args[url_placeholder_name]) {
+        throw new Error(
+          "Missing argument for the placeholder " + url_placeholder
+        )
+      }
+
+      path = path.replace(url_placeholder, args[url_placeholder_name])
+    })
+
+    return path
   }
 
   seneca.prepare(async function(this: any) {
