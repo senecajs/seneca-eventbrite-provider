807116c9f6f716304df3313f83101c66cb12a9d0
Gustavo <guhmerces@hotmail.com>
2022-01-19 06:16:57 -0300

    Add mock data to mocks.ts

--- test/mocks.ts
+++ test/mocks.ts
@@ -3,13 +3,13 @@ const mocks = {
     get: {
       url: "/events/:event_id/",
       mock_data: {
-        id: "214728557897",
+        id: "251037679457",
       },
     },
     post: {
       url: "/events/:event_id/",
       mock_data: {
-        id: "214728557897",
+        id: "251037679457",
       },
     },
   },
@@ -18,6 +18,27 @@ const mocks = {
       url: "/discounts/:discount_id/",
       mock_data: {
         id: "609282579",
+      },
+    },
+    post: {
+      url: "/discounts/:discount_id/",
+      mock_data: {
+        id: "609282579",
+      },
+    },
+  },
+  inventory_tier: {
+    get: {
+      url: "/events/:event_id/inventory_tiers/:inventory_tier/",
+      mock_data: {
+        id: "11195969",
+        event_id: "238083523227",
+      },
+    },
+    post: {
+      url: "/events/:event_id/inventory_tiers/:inventory_tier/",
+      mock_data: {
+        id: "11195969",
       },
     },
   },
