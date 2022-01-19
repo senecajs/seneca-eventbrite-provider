d343322a2beb099f4cd26bf2131910498fd0fb01
Gustavo <guhmerces@hotmail.com>
2022-01-19 06:09:03 -0300

    Create build_body_recursively()
    Iterate through body specification obj and create a new request body

--- src/cmd-handlers.ts
+++ src/cmd-handlers.ts
@@ -56,6 +56,36 @@ function cmd_handlers(initialized_req_handler: CallableFunction, body_args: Arra
     return this.make$(msg.ent.entity$).data$(res)
   }
 
+  function build_body_recursively(body_args: any, data_sources: any, body: any) {
+    Object.keys(body_args).forEach(body_arg_key => {
+      const pattern = body_args[body_arg_key]
+
+      if(typeof pattern === 'object' ) {
+        const recursive_body = {}
+        const data = build_body_recursively(pattern, data_sources, recursive_body)
+        body[body_arg_key] = data
+        return
+      }
+
+      const patterns = pattern.split('.')
+
+      const data_source = patterns.splice(0, 1)
+      const attrs = patterns.splice(0)
+
+      const data_source_name = data_source[0].replace(':', '')
+
+      if(!data_source_name) {
+        throw new Error('Unknown data_source ' + data_source_name)
+      }
+
+      const from = data_sources[data_source_name]
+      const data = set_attributes(attrs, from)
+
+      body[body_arg_key] = data
+    })
+    return body
+  }
+
   function set_attributes(attributes: Array<string>, data_source: Record<string,any>) {
     const data = attributes.reduce((previous: Record<string, any>, current: any, index, arr) => {
       if(!current || !previous) {
