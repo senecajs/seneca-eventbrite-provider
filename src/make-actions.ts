37840990be2659f08a14d56916f5b9b35badbc5c
Gustavo <guhmerces@hotmail.com>
2022-02-03 11:36:14 -0300

    Add the initial load() implementation

--- src/make-actions.ts
+++ src/make-actions.ts
@@ -5,6 +5,13 @@ function make_actions(action_data: ActionData) {
   const { path } = request
 
   async function load(this:any, msg:any) {
+    const { q } = msg
+
+    const built_path = build_path(path, q)
+
+    const res =  await req_fn(built_path)
+
+    return this.make$(msg.ent.entity$).data$(res)
   } 
 
   async function save(this:any, msg:any) {
