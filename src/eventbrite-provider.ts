4ab5ab76d317a6da720f9c1bee6f3d0174b74f61
Gustavo <guhmerces@hotmail.com>
2022-02-03 11:26:02 -0300

    Create add_actions() and add line calling it

--- src/eventbrite-provider.ts
+++ src/eventbrite-provider.ts
@@ -3,6 +3,7 @@
 import Eventbrite from 'eventbrite'
 import { Sdk } from 'eventbrite/lib/types'
 import { entities } from './entities'
+import { make_actions } from './handlers'
 import { make_request } from './make-request'
 import { ActionData, EntityMap } from './types'
 
@@ -16,9 +17,69 @@ function EventbriteProvider(this: any, options: any) {
 
   let eventbrite: Sdk
 
+  add_actions()
   seneca
     .message('role:entity,cmd:save,zone:provider,base:eventbrite,name:event', saveEvent)
 
+  function add_actions() {
+    const actions = prepare_actions(entities)
+
+    for (const action of actions) {
+      switch (action.pattern.cmd) {
+        case 'load':
+          seneca.message(action.pattern, make_load(action))
+          break
+      
+        case 'save':
+          seneca.message(action.pattern, make_save(action))
+          break
+      }
+    }
+
+  }
+
+  function make_load(action: ActionData) {
+    return make_actions(action)['load']
+  }
+
+  function make_save(action: ActionData) {
+    return make_actions(action)['save']
+  }
+
+
+  // prepare links replacing placeholders
+  function prepare_actions(entities: EntityMap): ActionData[] {
+    const actions_details: ActionData[] = []
+
+    for(const [ent_name, ent_details] of Object.entries(entities)) {
+      ent_details.name = ent_name
+
+      for(const [action_name, action_data] of Object.entries(ent_details.actions)) {
+
+        const pattern = {
+          name: ent_name,
+          cmd: action_name,
+          zone: 'provider',
+          base: 'eventbrite',
+          role: 'entity',
+        }
+
+        actions_details.push({
+          pattern,
+          req_fn: prepare_req_fn(action_data.request.method),
+          ...action_data
+        })
+      }
+
+    }
+    return actions_details
+  }
+
+  function prepare_req_fn(method: string) {
+    return (path: string, options?: Record<string, any>) => {
+      return make_request(eventbrite.request, path, method, options)
+    }
+  } 
 
   seneca.prepare(async function(this: any) {
     let out = await this.post('sys:provider,get:key,provider:eventbrite,key:api')
