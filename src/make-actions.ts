e56212853737fce21d275785448f866f1c2aaab4
Gustavo <guhmerces@hotmail.com>
2022-02-03 11:35:23 -0300

    Remove unused variables from make-actions.ts

--- src/make-actions.ts
+++ src/make-actions.ts
@@ -1,8 +1,8 @@
 import { ActionData } from "./types"
 
 function make_actions(action_data: ActionData) {
-  const { req_fn, request, after, before } = action_data
-  const { path, body_spec } = request
+  const { req_fn, request } = action_data
+  const { path } = request
 
   async function load(this:any, msg:any) {
   } 
