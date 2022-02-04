4e8479efae73c9ebcfd11ff1e66c990c185f3c5b
Gustavo <guhmerces@hotmail.com>
2022-02-04 10:58:54 -0300

    Add perform_task() to load() in make-actions.ts

--- src/make-actions.ts
+++ src/make-actions.ts
@@ -1,17 +1,36 @@
 import { ActionData } from "./types"
+import { perform_tasks } from "./utils"
 
 function make_actions(action_data: ActionData) {
-  const { req_fn, request } = action_data
+  const { req_fn, request, after, before } = action_data
   const { path } = request
 
   async function load(this:any, msg:any) {
     const { q } = msg
 
+    const context = {
+      query: q,
+    }
+
+    if(before) {
+      perform_tasks(before, context)
+    }
+
     const built_path = build_path(path, q)
 
     const res =  await req_fn(built_path)
 
-    return this.make$(msg.ent.entity$).data$(res)
+    const outent = this.make$(msg.ent.entity$).data$(res)
+
+    if(after) {
+      perform_tasks(after, {
+        res,
+        outent,
+        ...context
+      })
+    }
+
+    return outent
   } 
 
   async function save(this:any, msg:any) {
