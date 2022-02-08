2933aae558283bd2a8822c4e7033b1ecc21514e2
Gustavo <guhmerces@hotmail.com>
2022-02-08 12:37:24 -0300

    Add after prop to event load action in entities.ts

--- src/entities.ts
+++ src/entities.ts
@@ -8,8 +8,42 @@ const entities: EntityMap = {
           method: "get",
           path: "/events/:event_id/",
         },
+        after: [
+          { on: 'outent', field: 'event_id', set: { query: 'event_id' } }
+        ]
+      },
+      save: {
+        request: {
+          method: "post",
+          path: "/events/:event_id/",
+          body: {
+            event: [
+              'name',
+              'description', 
+              'start', 
+              'end', 
+              'currency',
+              'online_event',
+              'organizer_id',
+              'listed',
+              'shareable', 
+              'invite_only', 
+              'show_remaining',
+              'password',
+              'capacity',
+              'is_reserved_seating',
+              'is_series', 
+              'show_pick_a_seat',
+              'show_seatmap_thumbnail',
+              'show_colors_in_seatmap_thumbnail',
+            ]
+          },
+        },
         before: [
-          { on:'query', field: 'attribute', set: { query: 'event_id' } }
+          { on: 'req', del: 'event.start.local' },
+          { on: 'req', del: 'event.end.local' },
+          { on: 'req', del: 'event.name.text' },
+          { on: 'req', del: 'event.description.text' },
         ]
       }
     },    
