874fd0ed645ddc5336daf8cd4efe3d5b703fcc39
Gustavo <guhmerces@hotmail.com>
2022-02-08 11:42:19 -0300

    Add save() implementation to make-actions.ts

--- src/make-actions.ts
+++ src/make-actions.ts
@@ -1,4 +1,4 @@
-import { ActionData } from "./types"
+import { ActionData, Context } from "./types"
 import { perform_tasks } from "./utils"
 
 function make_actions(action_data: ActionData) {
@@ -8,7 +8,7 @@ function make_actions(action_data: ActionData) {
   async function load(this:any, msg:any) {
     const { q } = msg
 
-    const context = {
+    const context: Context = {
       query: q,
     }
 
@@ -34,7 +34,51 @@ function make_actions(action_data: ActionData) {
   } 
 
   async function save(this:any, msg:any) {
+    const { q, ent } = msg
+    let body: Record<string, any> = {}
 
+    let context: Context = {
+      query: q,
+      inent: ent,
+    }
+
+    const built_path = build_path(path, ent)
+    
+    if(action_data.request.body) {
+      body = fill_body(action_data.request.body, ent)
+    }
+
+    if(before) {
+      context = perform_tasks(before, {...context, req: body})
+    }
+
+    const res =  await req_fn(built_path, {
+      body: JSON.stringify(context.req)
+    })
+
+    const outent = this.make$(msg.ent.entity$).data$(res)
+
+    if(after) {
+      perform_tasks(after, {
+        res,
+        outent,
+        ...context
+      })
+    }
+
+    return outent
+  }
+
+  function fill_body(body_specs: Record<string, Array<string>>, entity: Record<string, any>) {
+    let body: Record<string, any> = {}
+    for(const [key, body_args] of Object.entries(body_specs)) {
+      body[key] = {}
+      body_args.forEach(attr => {
+        body[key][attr] = entity[attr] 
+      })  
+    }
+
+    return body
   }
 
   function build_path(path: string, args: Record<string, any>) {
