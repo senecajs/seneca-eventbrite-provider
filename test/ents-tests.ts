08a858e650fce73e37b58fb0ffe6e83f3104badb
Gustavo <guhmerces@hotmail.com>
2022-02-09 18:37:25 -0300

    Add entities data for testing and mock data

--- test/ents-tests.ts
+++ test/ents-tests.ts
@@ -1,7 +1,13 @@
 import { TestEntityMap } from "./types"
 
-let test_args = {
-  event_id: '238083523227'
+const test_args = {
+  event_id: '238083523227', //238083523227 251037679457
+  inventory_tier: '11195969',
+  discount_id: '609282579',
+  category_id: '101',
+  team_id: '1234',
+  attendee_id: '1234',
+  organization_id: '39198188963 ',
 }
 
 const ents_tests: TestEntityMap = {
@@ -35,6 +41,104 @@ const ents_tests: TestEntityMap = {
       },
     },
   },
+  category: {
+    load: {
+      args: {
+        category_id: test_args.category_id,
+      },
+      expectations: {
+        id: {
+          sameAs: test_args.category_id,
+        },
+      },
+    },
+  },
+  discount: {
+    load: {
+      args: {
+        discount_id: test_args.discount_id,
+      },
+      expectations: {
+        id: {
+          sameAs: test_args.discount_id,
+        },
+      },
+    },
+    save: {
+      changes: {
+        code: 'democode',
+      },
+      expectations: {
+        id: {
+          sameAs: test_args.discount_id,
+        },
+        code: {
+          sameAs: 'democode'
+        }
+      },
+    },
+  },
+  inventory_tier: {
+    load: {
+      args: {
+        event_id: test_args.event_id,
+        inventory_tier: test_args.inventory_tier
+      },
+      expectations: {
+        id: {
+          sameAs: test_args.inventory_tier,
+        },
+        event_id: {
+          sameAs: test_args.event_id,
+        },
+      },
+    },
+    save: {
+      changes: {
+        name: 'random',
+      },
+      expectations: {
+        id: {
+          sameAs: test_args.inventory_tier,
+        },
+        event_id: {
+          sameAs: test_args.event_id,
+        },
+      },
+    },
+  },
+  event_team: {
+    load: {
+      args: {
+        event_id: test_args.event_id,
+        team_id: test_args.team_id,
+      },
+      expectations: {
+        id: {
+          sameAs: test_args.team_id,
+        },
+        event_id: {
+          sameAs: test_args.event_id,
+        },
+      },
+    },
+  },
+  attendee: {
+    load: {
+      args: {
+        event_id: test_args.event_id,
+        attendee_id: test_args.attendee_id,
+      },
+      expectations: {
+        id: {
+          sameAs: test_args.attendee_id,
+        },
+        event_id: {
+          sameAs: test_args.event_id,
+        },
+      },
+    },
+  }
 }
 
 export { ents_tests }
--- test/mocks.ts
+++ test/mocks.ts
@@ -15,6 +15,69 @@ const mocks = {
       },
     },
   },
+  category: {
+    get: {
+      url: "/categories/:category_id/",
+      mock_data: {
+        id: "101",
+      },
+    },
+  },
+  discount: {
+    get: {
+      url: "/discounts/:discount_id",
+      mock_data: {
+        id: '609282579',
+      }
+    },
+    post: {
+      url: "/discounts/:discount_id",
+      mock_data: {
+        id: '609282579',
+        code: 'democode'
+      }
+    }
+  },
+  inventory_tier: {
+    get: {
+      url: "/events/:event_id/inventory_tiers/:inventory_tier/",
+      mock_data: {
+        inventory_tier: {
+          id: '11195969',
+          event_id: '238083523227',
+          name: '26ebbd125e7ccc13ee6f',
+        }
+      }
+    },
+    post: {
+      url: "/events/:event_id/inventory_tiers/:inventory_tier/",
+      mock_data: {
+        inventory_tier: {
+          id: '11195969',
+          event_id: '238083523227',
+          name: '26ebbd125e7ccc13ee6f',
+        }
+      }
+    },
+  },
+  event_team: {
+    get: {
+      url: "/events/:event_id/teams/:team_id",
+      mock_data: {
+        id: "1234",
+        event_id: "238083523227",
+      },
+    },
+  },
+  attendee: {
+    get: {
+      url: "/events/:event_id/attendees/:attendee_id/",
+      mock_data: {
+        id: "1234",
+        event_id: "238083523227"
+      },
+    },
+  }
 }
 
 export { mocks }
